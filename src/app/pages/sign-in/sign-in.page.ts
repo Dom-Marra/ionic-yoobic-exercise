da9a02b38966fc656e3d10f94ab6fc7bdbf78f29
Dom-Marra <dominicmarra321@gmail.com>
2021-12-17 19:28:50 -0800

    create signin form
    create signin form layout
    create interactivity between form and auth service
    route users to next view on a successful login

--- src/app/pages/sign-in/sign-in.page.ts
+++ src/app/pages/sign-in/sign-in.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInPage implements OnInit {
 
-  constructor() { }
+  public user = new FormGroup({                             //Login form controls
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
+
+  public isError: boolean = false;      //Whether there are login errors
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  /**
+   * Attempts to login the user provided the information in the login form
+   */
+  public login(): void {
+    this.authService.login(this.user.value).then(() => {
+      this.router.navigateByUrl('/menu');
+    }).catch(() => {
+      this.isError = true;
+    });
   }
 
 }
