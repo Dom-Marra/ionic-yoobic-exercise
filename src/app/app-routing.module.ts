eebbb6783d8b3076f037fdfadb25a13f34650e67
Dom-Marra <dominicmarra321@gmail.com>
2021-12-20 10:09:55 -0800

    add signed in guard to sign-in page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
+import { SignedInGuard } from './guards/signed-in.guard';
 
 const routes: Routes = [
   {
@@ -10,13 +11,15 @@ const routes: Routes = [
   },
   {
     path: 'sign-in',
-    loadChildren: () => import('./pages/sign-in/sign-in.module').then( m => m.SignInPageModule)
+    loadChildren: () => import('./pages/sign-in/sign-in.module').then( m => m.SignInPageModule),
+    canActivate: [SignedInGuard]
   },
   {
     path: 'menu',
     loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule),
     canActivate: [AuthGuard]
-  }
+  },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
