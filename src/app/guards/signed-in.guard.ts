652747482b7ca69c903ef6ca022387a2bd815525
Dom-Marra <dominicmarra321@gmail.com>
2021-12-20 11:08:45 -0800

    remove re-route
    do not need to re-route, this route is only activated by authguard

--- src/app/guards/signed-in.guard.ts
+++ src/app/guards/signed-in.guard.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignedInGuard implements CanActivate {
-  constructor (private authService: AuthService, private router: Router) {}
+  constructor (private authService: AuthService) {}
   
   canActivate(): boolean {
-    if (this.authService.getIsLoggedIn()) this.router.navigateByUrl("");
     return !this.authService.getIsLoggedIn();
   }
   
