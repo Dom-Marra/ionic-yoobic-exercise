f0ef7427bc7ba52140eea1a8a7f919508428b7f0
Dom-Marra <dominicmarra321@gmail.com>
2021-12-20 13:32:13 -0800

    re-add re-route if logged in
    since we're using localstorage now we need the re-route

--- src/app/guards/signed-in.guard.ts
+++ src/app/guards/signed-in.guard.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignedInGuard implements CanActivate {
-  constructor (private authService: AuthService) {}
+  constructor (private authService: AuthService, private router: Router) {}
   
   canActivate(): boolean {
+    if (this.authService.getIsLoggedIn()) this.router.navigateByUrl("", {replaceUrl: true});
     return !this.authService.getIsLoggedIn();
   }
   
